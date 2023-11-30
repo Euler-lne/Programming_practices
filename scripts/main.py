ba41dc1ab11a10e986c5621060e17f4edafa96a5
Euler_lne <3149512122@qq.com>
2023-12-01 00:30:20 +0800

    写好注释

--- scripts/main.py
+++ scripts/main.py
@@ -2,10 +2,9 @@ import compiler.compile as compile
 
 
 def main():
-    """_summary_ 主函数
-    """
+    """主函数"""
     encoding = input("请输入文件的编码：")
-    compiler = compile.Compiler("./data/商场客服机器人.txt",encodeing=encoding)
+    compiler = compile.Compiler("./data/商场客服机器人.txt", encodeing=encoding)
     compiler.saveToken("./log/token")
 
 
